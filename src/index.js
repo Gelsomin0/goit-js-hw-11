8686a5d1ff0d95a31797e008337e93795b562a48
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-06-08 21:19:34 +0300

    search and markup done

--- src/index.js
+++ src/index.js
@@ -9,3 +9,53 @@ const ell = {
     loadMoreBtn: document.querySelector('.load-more'),
 }
 
+ell.searchForm.addEventListener('submit', doSearchRequest);
+
+
+function doSearchRequest(e) {
+    e.preventDefault();
+
+    const searchQuery = ell.searchForm.querySelector('[name="searchQuery"]').value;
+
+    ell.galleryEl.innerHTML = '';
+
+    fetchSearchingData(searchQuery);
+}
+
+async function fetchSearchingData(searchQuery) {
+    const FETCH_URL = `${BASE_URL}?key=${API_KEY}&page=1&per_page=10&q=${searchQuery}`;
+    const fetchData = await axios.get(FETCH_URL);
+    const gettedData = createGalleryOfDataMarkup(fetchData.data);
+
+    ell.galleryEl.insertAdjacentHTML('beforeend', gettedData);
+}
+
+function createGalleryOfDataMarkup({hits, totalHits}) {
+    return hits.map((element) => {
+        const { likes, views, downloads, comments, previewURL } = element;
+        console.log(element);
+        return `
+            <div class="photo-card">
+                <img class='image' src="${previewURL}" alt="" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes:</b>
+                        <span>${likes}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Views:</b>
+                        <span>${views}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Comments:</b>
+                        <span>${comments}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads:</b>
+                        <span>${downloads}</span>
+                    </p>
+                </div>
+            </div>
+        `;
+    }).join('');
+}
